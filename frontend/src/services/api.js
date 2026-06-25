72012122f48a9aab9c96072eb91ade66ad27a534
Washimakhtar786 <washimwdn@gmail.com>
2026-06-25 15:54:14 +0530

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
   baseURL:
-    "http://localhost:4000",
+    "https://messmate-backend-ezo9.onrender.com",
 
   headers: {
     "Content-Type":
